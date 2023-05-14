47655c47e5a10e31eaaef20ce31328cfe83b228b
Rhuan Guilherme <rhuan23032004@gmail.com>
2023-05-13 21:22:49 -0300

    Criando Navbar :art:

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+  import NavbarComponentVue from './components/NavbarComponent.vue';
   import { onMounted } from 'vue'
   import { initFlowbite } from 'flowbite'
+
   onMounted(() => {
       initFlowbite();
   })
 </script>
 
 <template>
-  
+  <NavbarComponentVue/>
   <RouterView />
 </template>
 
