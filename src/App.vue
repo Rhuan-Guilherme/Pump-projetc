3856f897edcd0b33b97fd600a46e11849ffd96ad
Rhuan Guilherme <rhuan23032004@gmail.com>
2023-05-13 22:05:08 -0300

    Criando card de registro :art:

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
   import NavbarComponentVue from './components/NavbarComponent.vue';
   import TheForm from './components/TheForm.vue'
+  import TheCardVue from './components/TheCard.vue';
   import { onMounted } from 'vue'
   import { initFlowbite } from 'flowbite'
+
 
   onMounted(() => {
       initFlowbite();
@@ -11,8 +13,11 @@
 
 <template>
   <NavbarComponentVue/>
-  <div class="flex items-center justify-center mt-32 pb-20 border-b">
+  <div class="flex items-center justify-center mt-32 pb-20 border-b mb-8">
     <the-form class="w-2/4" />
+  </div>
+  <div class="flex items-center justify-center ">
+    <TheCardVue class="w-5/6"/>
   </div>
   <RouterView />
 </template>
---
+++ src/components/TheCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex flex-wrap gap-8 items-center justify-center">
+        <div class="w-[290px] h-[360px] border rounded-md shadow-sm px-2">
+            <div class="w-full text-lg font-medium flex items-center justify-center p-2 border-b">
+                Rhuan - 3416
+            </div>
+            <div class="flex flex-col gap-2 my-3">
+               <p>Prezados, o Sr. Rhuan entrou em contato solicitando a habilitação de um ponto de rede.</p> 
+               <p>Nome: Rhuan Guilherme de Sousa</p>
+               <p>Login: rhuan.g.silva</p>
+               <p>Local: Anexo 1A / 2° andar / sala 202</p>
+               <p>Ramal: 3416</p>
+               <p>Patrimônio: ETU087672</p>
+            </div>
+            <div class="w-full text-lg font-medium flex items-center justify-center p-2 border-t gap-2">
+                <div>
+                    <button type="button" class="px-11 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-md hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-red-800 dark:focus:ring-red-800 transition-all">Excluir</button>
+                </div>
+                <div>
+                    <button type="button" class="px-11 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-md hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Copiar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
