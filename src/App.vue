8189c9b80be21d8af71e7647bf9dfce36c34a795
Rhuan Guilherme <rhuan23032004@gmail.com>
2023-05-13 21:13:48 -0300

    Adicionando biblioteca tailwind Flowbite :arrow_up:

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-
+  import { onMounted } from 'vue'
+  import { initFlowbite } from 'flowbite'
+  onMounted(() => {
+      initFlowbite();
+  })
 </script>
 
 <template>
-
+  
   <RouterView />
 </template>
 
